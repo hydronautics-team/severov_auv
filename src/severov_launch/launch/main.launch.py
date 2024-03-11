50200db0f4fb41b93b5e93cae6a5c47a517a528f
Vladislav Plotnikov <vladik1209@gmail.com>
2024-03-12 01:32:49 +0300

    tested main launch

--- src/severov_launch/launch/main.launch.py
+++ src/severov_launch/launch/main.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
         "front_camera_topic", default_value='/stingray/topics/front_camera'
     )
     front_camera_path_arg = DeclareLaunchArgument(
-        "front_camera_path", default_value='/dev/video4'
+        "front_camera_path", default_value='/dev/video0'
     )
     transition_srv_arg = DeclareLaunchArgument(
         "transition_srv", default_value='/stingray/services/transition'
@@ -72,63 +72,63 @@ def generate_launch_description():
         #         'front_camera_topic': LaunchConfiguration("front_camera_topic"),
         #     }.items(),
         # ),
-        # Node(
-        #     package='stingray_missions',
-        #     executable='fsm_node',
-        #     name='fsm_node',
-        #     parameters=[
-        #         {'transition_srv': LaunchConfiguration("transition_srv")},
-        #         {'twist_action': LaunchConfiguration("twist_action")},
-        #         {'reset_imu_srv': LaunchConfiguration("reset_imu_srv")},
-        #         {'set_stabilization_srv': LaunchConfiguration(
-        #             "set_stabilization_srv")},
-        #     ],
-        #     respawn=True,
-        #     respawn_delay=1,
-        # ),
-        # Node(
-        #     package='stingray_movement',
-        #     executable='twist_action_server',
-        #     name='twist_action_server',
-        #     respawn=True,
-        #     respawn_delay=1,
-        # ),
-        # Node(
-        #     package='stingray_missions',
-        #     executable='qr_trigger_node',
-        #     name='qr_trigger_node',
-        #     parameters=[
-        #         {'transition_srv': LaunchConfiguration("transition_srv")},
-        #         {'zbar_topic': LaunchConfiguration("zbar_topic")}
-        #     ],
-        #     respawn=True,
-        #     respawn_delay=1,
-        # ),
-        # Node(
-        #     package='zbar_ros',
-        #     executable='barcode_reader',
-        #     name='qr_reader',
-        #     remappings=[
-        #         ('/image', LaunchConfiguration("front_camera_topic")),
-        #         ('/barcode', LaunchConfiguration("zbar_topic")),
-        #     ],
-        #     respawn=True,
-        #     respawn_delay=1,
-        # ),
-        # Node(
-        #     package='usb_cam',
-        #     executable='usb_cam_node_exe',
-        #     name='camera_driver',
-        #     remappings=[
-        #         ('/image_raw', LaunchConfiguration("front_camera_topic")),
-        #     ],
-        #     parameters=[
-        #         {'video_device': LaunchConfiguration("front_camera_path")},
-        #         # {'camera_name': "/front"},
-        #     ],
-        #     respawn=True,
-        #     respawn_delay=1,
-        # ),
+        Node(
+            package='stingray_missions',
+            executable='fsm_node',
+            name='fsm_node',
+            parameters=[
+                {'transition_srv': LaunchConfiguration("transition_srv")},
+                {'twist_action': LaunchConfiguration("twist_action")},
+                {'reset_imu_srv': LaunchConfiguration("reset_imu_srv")},
+                {'set_stabilization_srv': LaunchConfiguration(
+                    "set_stabilization_srv")},
+            ],
+            respawn=True,
+            respawn_delay=1,
+        ),
+        Node(
+            package='stingray_movement',
+            executable='twist_action_server',
+            name='twist_action_server',
+            respawn=True,
+            respawn_delay=1,
+        ),
+        Node(
+            package='stingray_missions',
+            executable='qr_trigger_node',
+            name='qr_trigger_node',
+            parameters=[
+                {'transition_srv': LaunchConfiguration("transition_srv")},
+                {'zbar_topic': LaunchConfiguration("zbar_topic")}
+            ],
+            respawn=True,
+            respawn_delay=1,
+        ),
+        Node(
+            package='zbar_ros',
+            executable='barcode_reader',
+            name='qr_reader',
+            remappings=[
+                ('/image', LaunchConfiguration("front_camera_topic")),
+                ('/barcode', LaunchConfiguration("zbar_topic")),
+            ],
+            respawn=True,
+            respawn_delay=1,
+        ),
+        Node(
+            package='usb_cam',
+            executable='usb_cam_node_exe',
+            name='camera_driver',
+            remappings=[
+                ('/image_raw', LaunchConfiguration("front_camera_topic")),
+            ],
+            parameters=[
+                {'video_device': LaunchConfiguration("front_camera_path")},
+                # {'camera_name': "/front"},
+            ],
+            respawn=True,
+            respawn_delay=1,
+        ),
         # Node(
         #     package='rqt_gui',
         #     executable='rqt_gui',
