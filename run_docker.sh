b86d30dc3fbd80849f64fbb04fc348257f56cb56
Vladislav Plotnikov <vladik1209@gmail.com>
2024-03-20 00:47:18 +0300

    update run docker script

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
-docker run --rm -it \
+docker run --rm -it --gpus all\
     -v $HOME/severov_auv:/severov_auv \
     --device=/dev/ttyTHS0 \
     --device=/dev/video0 \
     --net=host \
-    severov_auv:latest \
+    hydronautics/severov_auv:latest \
     bash
