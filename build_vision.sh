329d81ce4ef12666e94d57b4ba32306c9f5fd76e
Vladislav Plotnikov <vladik1209@gmail.com>
2024-03-30 17:33:28 +0300

    fix script

--- build_vision.sh
+++ build_vision.sh
@@ -1,1 +1,1 @@
-colcon build --packages-select sauvc_object_detection stingray_object_detection stingray_interfaces severov_launch sauvc_launch stingray_launch
+colcon build --packages-select sauvc_object_detection stingray_object_detection stingray_interfaces severov_launch sauvc_launch stingray_launch severov_cam
