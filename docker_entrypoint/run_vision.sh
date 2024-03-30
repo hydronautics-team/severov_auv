270bfd1de389257931a7af278d5938346281e52a
Vladislav Plotnikov <vladik1209@gmail.com>
2024-03-30 14:49:12 +0300

    add obj coords calculation

--- docker_entrypoint/run_vision.sh
+++ docker_entrypoint/run_vision.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 source install/setup.bash && ros2 launch severov_launch vision.launch.py
---
+++ run_vision_v5.sh
@@ -0,0 +1,1 @@
+source install/setup.bash && ros2 launch severov_launch vision_yolov5.launch.py
